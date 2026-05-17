497487b9f518ba65f55a478e64e14b8248890fd5
Adem Tiss <157023504+AdemTiss@users.noreply.github.com>
2026-05-17 17:03:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log('Hello git')
 console.log('Second line')
 console.log(' this is feauture 1' ) 
-console.log('this changement is made on github")
+console.log('this changement is made on github')
